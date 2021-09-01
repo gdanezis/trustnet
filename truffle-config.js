efa9c9c4ae1b9894b7694cc013c33e37e5310fe8
gdanezis <g.danezis@ucl.ac.uk>
2021-09-01 18:01:38 +0100

    Use internal truffle test network

--- truffle-config.js
+++ truffle-config.js
@@ -5,18 +5,18 @@ module.exports = {
   // see <http://truffleframework.com/docs/advanced/configuration>
   // for more details on how to specify configuration options!
   //
-  networks: {
-   development: {
-     host: "127.0.0.1",
-     port: 8545,
-     network_id: "*"
-   },
-   test: {
-     host: "127.0.0.1",
-     port: 8545,
-     network_id: "*"
-   }
-  },
+  // networks: {
+  //  development: {
+  //    host: "127.0.0.1",
+  //    port: 8545,
+  //    network_id: "*"
+  //  },
+  //  test: {
+  //    host: "127.0.0.1",
+  //    port: 8545,
+  //    network_id: "*"
+  //  }
+  // },
   //
   // Truffle DB is currently disabled by default; to enable it, change enabled:
   // false to enabled: true. The default storage location can also be
